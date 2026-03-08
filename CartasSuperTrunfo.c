1cb61cae1465d75bf4d06a4609f234e7b3128b9a
Perlamagna <202601269607@alunos.estacio.br>
2026-03-08 03:39:11 +0000

    Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,9 +9,11 @@ int main() {
     float pib;
     int pontosTuristicos;
 
+    float densidadePopulacional;
+    float pibPerCapita;
+
     printf("=== Cadastro de Carta - Super Trunfo Paises ===\n\n");
 
-    // Entrada de dados
     printf("Digite a letra do estado (A-H): ");
     scanf(" %c", &estado);
 
@@ -24,19 +26,26 @@ int main() {
     printf("Digite a area (km2): ");
     scanf("%f", &area);
 
-    printf("Digite o PIB (em bilhoes): ");
+    printf("Digite o PIB: ");
     scanf("%f", &pib);
 
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontosTuristicos);
 
-    // Saída de dados
+    // Calculos automáticos
+    densidadePopulacional = populacao / area;
+    pibPerCapita = pib / populacao;
+
     printf("\n=== Carta Cadastrada ===\n");
     printf("Codigo da carta: %c0%d\n", estado, cidade);
     printf("Populacao: %d\n", populacao);
     printf("Area: %.2f km2\n", area);
-    printf("PIB: %.2f bilhoes\n", pib);
+    printf("PIB: %.2f\n", pib);
     printf("Pontos Turisticos: %d\n", pontosTuristicos);
+
+    printf("\n--- Dados Calculados ---\n");
+    printf("Densidade Populacional: %.2f hab/km2\n", densidadePopulacional);
+    printf("PIB per Capita: %.6f\n", pibPerCapita);
 
     return 0;
 }
